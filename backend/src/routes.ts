99df8a508861db3b9af2e18b06b593af84405916
joaovds <jv782063@gmail.com>
2020-06-02 16:15:11 -0300

    Cria a funcionalidade de listagem de itens

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  res.send('Base server!');
+routes.get('/items', async (req, res) => {
+  const items = await knex('tb_items').select('*');
+
+  const serializedItems = items.map((item) => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    };
+  });
+
+  return res.json(serializedItems);
 });
 
 export default routes;
