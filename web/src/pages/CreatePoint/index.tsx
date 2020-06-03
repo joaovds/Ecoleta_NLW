a596252b8953e6ebfc171100e6704212b549e30f
joaovds <jv782063@gmail.com>
2020-06-03 17:46:54 -0300

    Busca e lista todos os estados e suas respectivas cidades da API do IBGE

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, ChangeEvent } from 'react';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
 import api from '../../services/api';
+import axios from 'axios';
 
 import './style.css';
 import logo from '../../assets/logo.svg';
@@ -14,14 +15,65 @@ interface Iitem {
   image_url: string;
 }
 
+interface IIBGEUFResponse {
+  sigla: string;
+}
+
+interface IIBGECITYResponse {
+  nome: string;
+}
+
 const CreatePoint = () => {
   const [items, setItems] = useState<Iitem[]>([]);
+  const [ufs, setUfs] = useState<string[]>([]);
+  const [selectedUf, setSelectedUf] = useState('0');
+  const [selectedCity, setSelectedCity] = useState('0');
+  const [cities, setCities] = useState<string[]>([]);
 
   useEffect(() => {
     api.get('items').then((res) => {
       setItems(res.data);
     });
   }, []);
+
+  useEffect(() => {
+    axios
+      .get<IIBGEUFResponse[]>(
+        'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
+      )
+      .then((res) => {
+        const ufInitials = res.data.map((uf) => uf.sigla);
+
+        setUfs(ufInitials);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (selectedUf === '0') {
+      return;
+    }
+
+    axios
+      .get<IIBGECITYResponse[]>(
+        `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`
+      )
+      .then((res) => {
+        const nameCity = res.data.map((city) => city.nome);
+
+        setCities(nameCity);
+      });
+  }, [selectedUf]);
+
+  function handleSelectedUf(e: ChangeEvent<HTMLSelectElement>) {
+    const uf = e.target.value;
+
+    setSelectedUf(uf);
+  }
+  function handleSelectedCity(e: ChangeEvent<HTMLSelectElement>) {
+    const city = e.target.value;
+
+    setSelectedCity(city);
+  }
 
   return (
     <div id='page-create-point'>
@@ -80,16 +132,36 @@ const CreatePoint = () => {
             <div className='field'>
               <label htmlFor='uf'>Estado</label>
 
-              <select name='uf' id='uf'>
-                <option value='SP'>Selecione uma UF</option>
+              <select
+                name='uf'
+                id='uf'
+                value={selectedUf}
+                onChange={handleSelectedUf}
+              >
+                <option value='0'>Selecione uma UF</option>
+                {ufs.map((uf) => (
+                  <option key={uf} value={uf}>
+                    {uf}
+                  </option>
+                ))}
               </select>
             </div>
 
             <div className='field'>
               <label htmlFor='city'>Cidade</label>
 
-              <select name='city' id='city'>
-                <option value='Peruba'>Selecione uma </option>
+              <select
+                name='city'
+                id='city'
+                value={selectedCity}
+                onChange={handleSelectedCity}
+              >
+                <option value='0'>Selecione uma Cidade</option>
+                {cities.map((city) => (
+                  <option key={city} value={city}>
+                    {city}
+                  </option>
+                ))}
               </select>
             </div>
           </div>
