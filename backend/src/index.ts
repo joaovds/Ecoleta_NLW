d910b1c351acbd9b092629d68325bb8b8129e8ce
joaovds <jv782063@gmail.com>
2020-06-07 09:57:21 -0300

    Cria a validação de entrada de dados

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,8 @@ import cors from 'cors';
 import routes from './routes';
 import path from 'path';
 
+import { errors } from 'celebrate';
+
 const app = express();
 
 app.use(cors());
@@ -10,5 +12,7 @@ app.use(express.json());
 app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 app.listen(3333);
